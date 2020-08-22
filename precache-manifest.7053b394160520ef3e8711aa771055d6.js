ffcb3ad0ef50e9e1a02f0f39bd176d829691959e
Martin Kero <martin.kero@gmail.com>
2020-08-22 21:12:24 +0200

    Updates

---
+++ precache-manifest.7053b394160520ef3e8711aa771055d6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5373ec12160c5a5e210f27f408fe2e3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fb78eb4321cb6bdce16",
+    "url": "/static/css/main.92a319ba.chunk.css"
+  },
+  {
+    "revision": "ed534123b4b596811f79",
+    "url": "/static/js/2.d018f4a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d018f4a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fb78eb4321cb6bdce16",
+    "url": "/static/js/main.d3fa942e.chunk.js"
+  },
+  {
+    "revision": "59526feca9b133dc985f",
+    "url": "/static/js/runtime-main.f0f3d5a0.js"
+  },
+  {
+    "revision": "449d681cd6006390e1bee3c3a660430b",
+    "url": "/static/media/open-sans-v17-latin-600.449d681c.woff"
+  },
+  {
+    "revision": "819af3d3abdc9f135d49b80a91e2ff4c",
+    "url": "/static/media/open-sans-v17-latin-600.819af3d3.woff2"
+  },
+  {
+    "revision": "33543c5cc5d88f5695dd08c87d280dfd",
+    "url": "/static/media/open-sans-v17-latin-regular.33543c5c.woff2"
+  },
+  {
+    "revision": "de0869e324680c99efa1250515b4b41c",
+    "url": "/static/media/open-sans-v17-latin-regular.de0869e3.woff"
+  }
+]);
