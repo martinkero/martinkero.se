2847ef32e225a8415bc8b9b5028ecb260e876fcf
Martin Kero <martin.kero@gmail.com>
2020-08-22 20:31:16 +0200

    Added target blank to travis link

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ import { Card } from './components/card/Card';
 const App = () =>
   <div className="app">
     <Card />
+    <footer>
+      <a href="https://travis-ci.org/github/martinkero/martinkero.se" target="_blank">Travis CI</a>
+    </footer>
   </div>
 
 export default App;
