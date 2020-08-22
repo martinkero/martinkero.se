2b0112c0136a96363edc6168c0293a42d42d78af
Martin Kero <martin.kero@gmail.com>
2020-08-22 19:34:28 +0200

    portfolio-fe initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Card } from './components/card/Card';
+
+const App = () =>
+  <div className="app">
+    <Card />
+  </div>
+
+export default App;
