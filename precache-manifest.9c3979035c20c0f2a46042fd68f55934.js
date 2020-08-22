2c320e4ca7ee8553597782e1efc2048ce768afa6
Martin Kero <martin.kero@gmail.com>
2020-08-22 19:32:57 +0200

    Updates

---
+++ precache-manifest.9c3979035c20c0f2a46042fd68f55934.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7930ccc83e5808f43891b59bc63b6e3f",
+    "url": "/portfolio-fe/index.html"
+  },
+  {
+    "revision": "ebaec14be3e56b99fe38",
+    "url": "/portfolio-fe/static/css/main.88665c73.chunk.css"
+  },
+  {
+    "revision": "ba30e1126a8e3c9c0dbe",
+    "url": "/portfolio-fe/static/js/2.1c2bba13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-fe/static/js/2.1c2bba13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebaec14be3e56b99fe38",
+    "url": "/portfolio-fe/static/js/main.a217c981.chunk.js"
+  },
+  {
+    "revision": "2e34e8b34670d1788bde",
+    "url": "/portfolio-fe/static/js/runtime-main.9017d80c.js"
+  },
+  {
+    "revision": "449d681cd6006390e1bee3c3a660430b",
+    "url": "/portfolio-fe/static/media/open-sans-v17-latin-600.449d681c.woff"
+  },
+  {
+    "revision": "819af3d3abdc9f135d49b80a91e2ff4c",
+    "url": "/portfolio-fe/static/media/open-sans-v17-latin-600.819af3d3.woff2"
+  },
+  {
+    "revision": "33543c5cc5d88f5695dd08c87d280dfd",
+    "url": "/portfolio-fe/static/media/open-sans-v17-latin-regular.33543c5c.woff2"
+  },
+  {
+    "revision": "de0869e324680c99efa1250515b4b41c",
+    "url": "/portfolio-fe/static/media/open-sans-v17-latin-regular.de0869e3.woff"
+  }
+]);
