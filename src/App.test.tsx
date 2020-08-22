300651e558af8fcfdef898b698dcf08c5a9d8107
Martin Kero <martin.kero@gmail.com>
2020-08-22 20:18:32 +0200

    Fixed basic test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app with card', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const nameElement = getByText(/Martin Kero/i);
+  expect(nameElement).toBeInTheDocument();
 });
