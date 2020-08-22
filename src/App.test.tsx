c13d1144a0854c985c79411f47827c2942f86113
Martin Kero <martin.kero@gmail.com>
2020-08-22 23:06:48 +0200

    Updates

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders app with card', () => {
-  const { getByText } = render(<App />);
-  const nameElement = getByText(/Martin Kero/i);
-  expect(nameElement).toBeInTheDocument();
-});
